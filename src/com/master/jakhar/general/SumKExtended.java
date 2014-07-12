5e54256f11ebbb67d33526e60d216ca1b26ab961
PankaJ Jakhar <pankajjakhar5@gmail.com>
2014-07-12 13:35:55 +0530

    New test cases added for SumKExtended

--- src/com/master/jakhar/general/SumKExtended.java
+++ src/com/master/jakhar/general/SumKExtended.java
@@ -17,67 +17,85 @@ import java.util.HashMap;
  */
 public class SumKExtended {
 
-	
-	
 	public static void main(String[] args) {
-		int[] input = new int[] { 4, 4, 6, 6};
+		int[] input = new int[] { 4, 4, 6, 6 };
 		System.out.println("Test case 1 for Sum = 10>>>");
 		printOutput(findSum(input, 10));
 		System.out.println();
-		
+
 		input = new int[] { 5, -2, 3, 8, 7, 6, 4, 10, 1, 9 };
 		System.out.println("Test case 2 for Sum = 3>>>");
 		printOutput(findSum(input, 3));
 		System.out.println();
-		
+
 		input = new int[] { 1, -2, 3, 8, 7, 6, 4, 10, 1, 9 };
 		System.out.println("Test case 3 for Sum = 2>>>");
 		printOutput(findSum(input, 2));
 		System.out.println();
-		
+
 		input = new int[] { 1, -2, 3, 8, 7, 6, 4, 10, 1, 9 };
 		System.out.println("Test case 4 for Sum = 7>>>");
 		printOutput(findSum(input, 7));
 		System.out.println();
-		
+
 		input = new int[] { 2, -2, 2, 8, 7, 6, 4, 10, 1, 9 };
 		System.out.println("Test case 5 for Sum = 4>>>");
 		printOutput(findSum(input, 4));
+		System.out.println();
+
+		input = new int[] { 2, 2 };
+		System.out.println("Test case 6 for Sum = 4>>>");
+		printOutput(findSum(input, 4));
+		System.out.println();
+
+		input = new int[] { 2, 2, 2 };
+		System.out.println("Test case 7 for Sum = 4>>>");
+		printOutput(findSum(input, 4));
+		System.out.println();
+
+		input = new int[] { 4, 6, 4, 6 };
+		System.out.println("Test case 8 for Sum = 10>>>");
+		printOutput(findSum(input, 10));
+		System.out.println();
+
+		input = new int[] { 4, 6, 6, 6 };
+		System.out.println("Test case 9 for Sum = 10>>>");
+		printOutput(findSum(input, 10));
 		System.out.println();
 	}
 
 	/**
 	 * Prints the Output for Logging.
+	 * 
 	 * @param findSum
 	 */
 	private static void printOutput(ArrayList<Index> indicesList) {
 		int size = indicesList.size();
-		for(int i = 0; i < size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			Index index = indicesList.get(i);
-			System.out.printf("Combination #%d>>>>>\n", i + 1);
+			System.out.printf("Indices Pair #%d>>>>>\n", i + 1);
 			System.out.println(index.secondElement);
 			System.out.println(index.firstElement);
 		}
-		
+
 	}
 
 	/**
 	 * Stores the indices of items found in the array.
+	 * 
 	 * @author JAKHAR
 	 *
 	 */
-	public static class Index{
+	public static class Index {
 		int firstElement;
 		int secondElement;
-		
-		public Index(int first, int second)
-		{
+
+		public Index(int first, int second) {
 			firstElement = first;
 			secondElement = second;
 		}
 	}
-	
+
 	/**
 	 * Finds the two numbers in the array which has sum = k.
 	 * 
@@ -86,13 +104,13 @@ public class SumKExtended {
 	 */
 	public static ArrayList<Index> findSum(int[] input, int sum) {
 
-		/**List to store Index instances.*/
+		/** List to store Index instances. */
 		ArrayList<Index> indicesList = new ArrayList<Index>();
-		
+
 		/** Optimal Lookup table. */
 		HashMap<Integer, ArrayList<Integer>> valueToIndices = new HashMap<Integer, ArrayList<Integer>>();
 
-				int size = input.length;
+		int size = input.length;
 		for (int i = 0; i < size; i++) {
 			int currentValue = input[i];
 			/**
@@ -102,30 +120,30 @@ public class SumKExtended {
 			 */
 			if (valueToIndices.containsKey(sum - currentValue)) {
 				int currentIndex = i;
-				ArrayList<Integer> indices = valueToIndices.get(sum - currentValue);
-				
-				int storedIndicesSize = indices.size();
-				for(int indexCount = 0; indexCount < storedIndicesSize; indexCount++)
-				{
-					Index index = new Index(currentIndex, indices.get(indexCount));
-					indicesList.add(index);
+
+				/** Get stored indices list from Lookup table. */
+				ArrayList<Integer> storedIndices = valueToIndices.get(sum
+						- currentValue);
+
+				/** Now create new Index pairs with this current index. */
+				int storedIndicesSize = storedIndices.size();
+				for (int indexCount = 0; indexCount < storedIndicesSize; indexCount++) {
+					indicesList.add(new Index(currentIndex, storedIndices
+							.get(indexCount)));
 				}
 			}
-			
-			/**Stores current Index if duplicate or new one.*/
-			if(valueToIndices.containsKey(currentValue))
-			{
-				ArrayList<Integer> inddexList = valueToIndices.get(currentValue);
-				inddexList.add(i);
-				valueToIndices.put(currentValue, inddexList);
-			}
-			else
-			{
+
+			/** Stores current Index if duplicate or new one. */
+			if (valueToIndices.containsKey(currentValue)) {
+				ArrayList<Integer> indexList = valueToIndices.get(currentValue);
+				indexList.add(i);
+				valueToIndices.put(currentValue, indexList);
+			} else {
 				ArrayList<Integer> inddexList = new ArrayList<Integer>();
 				inddexList.add(i);
 				valueToIndices.put(currentValue, inddexList);
 			}
-			
+
 		}
 		return indicesList;
 	}
